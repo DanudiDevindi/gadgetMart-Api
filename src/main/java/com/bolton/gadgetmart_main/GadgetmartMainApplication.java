e05cca5102f6587d6f4b730ba350af7c1fb21aeb
Danudi <danusilva1997@gmail.com>
2022-01-10 14:55:40 +0530

    initial commit

---
+++ src/main/java/com/bolton/gadgetmart_main/GadgetmartMainApplication.java
@@ -0,0 +1,13 @@
+package com.bolton.gadgetmart_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetmartMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadgetmartMainApplication.class, args);
+	}
+
+}
