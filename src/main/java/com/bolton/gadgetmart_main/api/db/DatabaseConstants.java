147956b7dc75913b430cddefc743bd8779b68e68
Danudi <danusilva1997@gmail.com>
2022-01-11 09:25:26 +0530

    db connection

---
+++ src/main/java/com/bolton/gadgetmart_main/api/db/DatabaseConstants.java
@@ -0,0 +1,12 @@
+package com.bolton.gadgetmart_main.api.db;
+
+public interface DatabaseConstants {
+
+	   static final String DB_URL = "jdbc:mysql://localhost:3306/gagetmart_main";
+
+	    static final String DRIVER = "com.mysql.jdbc.Driver";
+
+	    static final String USERNAME = "root";
+
+	    static final String PASSWORD = ""; 
+}
